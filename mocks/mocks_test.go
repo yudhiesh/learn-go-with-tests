9a76ff42671e192f7b17e1a1db8091fa8931c45a
yudhiesh <yudhiesh1997@gmail.com>
2024-11-17 17:27:24 +0800

    feat: add in di and mock

---
+++ mocks/mocks_test.go
@@ -0,0 +1,58 @@
+package main
+
+import (
+	"bytes"
+	"slices"
+	"testing"
+	"time"
+)
+
+func TestCountdown(t *testing.T) {
+	t.Run("prints 3 -> Go!", func(t *testing.T) {
+		buffer := &bytes.Buffer{}
+		spySleeper := &SpySleeper{}
+
+		Countdown(buffer, spySleeper)
+
+		got := buffer.String()
+		want := `3
+2
+1
+Go!`
+
+		if got != want {
+			t.Errorf("got %q want %q", got, want)
+		}
+
+	})
+	t.Run("sleep before every print", func(t *testing.T) {
+		spySleepPrinter := &SpyCountdownOperations{}
+		Countdown(spySleepPrinter, spySleepPrinter)
+
+		want := []string{
+			write,
+			sleep,
+			write,
+			sleep,
+			write,
+			sleep,
+			write,
+		}
+
+		if !slices.Equal(want, spySleepPrinter.Calls) {
+			t.Errorf("wanted calls %v got %v", want, spySleepPrinter.Calls)
+		}
+	})
+	t.Run("configurable sleep of 2 seconds before every print", func(t *testing.T) {
+		sleepTime := 5 * time.Second
+
+		spyTime := &SpyTime{}
+		sleeper := ConfigurableSleeper{sleepTime, spyTime.Sleep}
+		sleeper.Sleep()
+
+		if spyTime.durationSlept != sleepTime {
+			t.Errorf("should have slept for %v but slept for %v", sleepTime, spyTime.durationSlept)
+		}
+
+	})
+}
